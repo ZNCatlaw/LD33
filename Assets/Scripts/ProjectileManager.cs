0dba51024aa624c1062cce09a46354e04257d91e
ccudahy <ccudahy@yahoo.com>
2015-08-22 12:15:52 -0800

    Projectile and controller work.

--- Assets/Scripts/ProjectileManager.cs
+++ Assets/Scripts/ProjectileManager.cs
@@ -5,12 +5,13 @@ public class ProjectileManager : MonoBehaviour
 {
     public GameObject m_LaserPrefab;
 
-
+    GameObject m_Projectiles;
 
     // Use this for initialization
     void Start()
     {
-
+        m_Projectiles = new GameObject();
+        m_Projectiles.transform.parent = this.transform;
     }
 
     // Update is called once per frame
@@ -18,13 +19,15 @@ public class ProjectileManager : MonoBehaviour
     {
         foreach (Transform child in transform)
         {
-            child.transform.Translate(new Vector3(0, 0.01f, 0));
+            float speed = 10f * Time.deltaTime;
+            child.transform.Translate(new Vector3(0, speed, 0));
         }
     }
 
     public void AddLaser( Vector3 position, Vector3 direction )
     {
         GameObject laser = Instantiate(m_LaserPrefab, position, transform.rotation) as GameObject;
-        laser.transform.parent = transform;
+        laser.transform.parent = m_Projectiles.transform;
+        DestroyObject(laser, 2.0f);
     }
 }
