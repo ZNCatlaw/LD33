c39eb9f0de6c2e7745893acdf67a1e1f76972670
various authors <various.authors@gmail.com>
2015-08-21 23:23:35 -0700

    made it rain with ship pumas

--- Assets/Scripts/ShipManager.cs
+++ Assets/Scripts/ShipManager.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class ShipManager : MonoBehaviour {
 
+	public GameObject[] shipTypes;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		
+	}
+
+	void Spawn () {
+
 	}
 }
---
+++ Assets/Scripts/StupidLinearShip.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StupidLinearShip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D body = this.GetComponent<Rigidbody2D> ();
+		body.velocity = new Vector2 (0, -1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
