38c7398e309ab136484354091c841838bc7946c7
ccudahy <ccudahy@yahoo.com>
2015-08-21 23:32:46 -0800

    Eyes

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerManager : MonoBehaviour {
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject m_PlayerPrefab;
+    Player[] m_Players;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+    void Start()
+    {
+        m_Players = new Player[1];
+
+        GameObject player = Instantiate(m_PlayerPrefab, transform.position, transform.rotation) as GameObject;
+        m_Players[0] = player.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
 	}
 }
