0987e426ac43f7cc220fd7e8611f18447dc4bda5
ccudahy <ccudahy@yahoo.com>
2015-08-21 21:51:30 -0800

    WIP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,30 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    GameObject m_EyePrefab;
-    GameObject m_Eyes;
+    public GameObject m_EyePrefab;
+    Eye[] m_Eyes;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-        m_Eyes[0] = Instantiate( m_EyePrefab, transform.position, transform.rotation) as Eye;
-        m_Eyes[1] = Instantiate(m_EyePrefab, transform.position, transform.rotation) as Eye;
+        m_Eyes = new Eye[1];
+        GameObject leftEye = Instantiate(m_EyePrefab, transform.position, transform.rotation) as GameObject;
+        leftEye.transform.parent = transform;
+        leftEye.transform.Translate( new Vector2(-1, 0));
+        m_Eyes[0] = leftEye.GetComponent<Eye>();
+
+        GameObject rightEye = Instantiate(m_EyePrefab, transform.position, transform.rotation) as GameObject;
+        rightEye.transform.parent = transform;
+        rightEye.transform.Translate(new Vector2(1, 0));
+        m_Eyes[1] = rightEye.GetComponent<Eye>();
     }
 
     // Update is called once per frame
     void Update ()
     {
-	
-	}
+        float horizontal = Input.GetAxis("LeftHorizontal");
+        float vertical = Input.GetAxis("LeftVertical");
+
+
+    }
 }
