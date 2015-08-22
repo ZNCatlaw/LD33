bfc8c4f9f737c15d2e76bf6b706d800ac3b693c3
ccudahy <ccudahy@yahoo.com>
2015-08-21 20:51:35 -0800

    Created Unity Prefabs

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour
+{
+    PlayerManager m_playerManager;
+    ProjectileManager m_projectileManager;
+    ShipManager m_shipManager;
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_playerManager = GetComponent<PlayerManager>();
+        m_projectileManager = GetComponent<ProjectileManager>();
+        m_shipManager = GetComponent<ShipManager>();
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
