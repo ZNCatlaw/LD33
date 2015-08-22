e222f813d9dd0c9451dc55c524e195d1128d1a89
various authors <various.authors@gmail.com>
2015-08-22 00:15:36 -0700

    Added Collision to ships
    - ships have a 2d circle collider
    - they bump each other, but the linear ship also ensures it is always
      pointing down towards the beast

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class Spawner : MonoBehaviour {
 
 	public GameObject[] m_shipTypes;
+	public float spawnTime;
+	public float spawnDelay;
 
 	// Use this for initialization
 	void Start () {
-		
+	
+		InvokeRepeating ("Spawn", spawnDelay, spawnTime);
 	}
 
 	void Spawn () {
 		int ship_type_index = Random.Range (0, m_shipTypes.Length);
 		float random_x = Random.Range (0f, 1f);
 
-		Vector2 position = Camera.main.ViewportToWorldPoint(new Vector2(random_x, 1));
+		Vector2 position = Camera.main.ViewportToWorldPoint(new Vector2(random_x, 1.1f));
 		
 		Instantiate (m_shipTypes [ship_type_index], position, Quaternion.identity);
 	}
@@ -22,6 +25,5 @@ public class Spawner : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		Spawn ();
 	}
 }
--- Assets/Scripts/StupidLinearShip.cs
+++ Assets/Scripts/StupidLinearShip.cs
@@ -6,11 +6,23 @@ public class StupidLinearShip : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Rigidbody2D body = this.GetComponent<Rigidbody2D> ();
-		body.velocity = new Vector2 (0, -1);
+		body.velocity = new Vector2 (0, -(1 + Random.Range(-0.1f, 0.1f)));
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		//create rotation
+		Vector2 target = Camera.main.ViewportToWorldPoint(new Vector2(0, -1));
+		Vector2 position = transform.position;
+
+		target.x = position.x; // ensure the ship is pointing straight ahead
+		Quaternion wantedRotation = Quaternion.LookRotation(target - position);
+
+		//then rotate
+		transform.rotation = Quaternion.Lerp(transform.rotation, wantedRotation, Time.time * 1);
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		Debug.Log("Collision detected with trigger object ");
 	}
 }
