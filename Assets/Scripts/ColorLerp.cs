c7b83bd3ea3abd809c055f9491467fc00b4b020e
various authors <various.authors@gmail.com>
2015-08-22 21:28:50 -0700

    Did it with shaders stolen from the web

---
+++ Assets/Scripts/ColorLerp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorLerp : MonoBehaviour {
+	public Material material1;
+	public Material material2;
+	public float duration = 2.0F;
+	public SpriteRenderer rend;
+
+	void Start() {
+		rend = GetComponent<SpriteRenderer>();
+		rend.material = material1;
+	}
+	void Update() {
+		float lerp = Mathf.PingPong(Time.time, duration) / duration;
+		rend.material.Lerp(material1, material2, lerp);
+	}
+}
