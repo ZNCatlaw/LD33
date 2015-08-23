67c70543d16e5e9ad92bccb5846e8722e3d34809
Tim Bellefleur <nomoon@phoebus.ca>
2015-08-22 17:06:43 -0700

    Got something working in this department, now to manage it!

--- Assets/Scripts/BackgroundBehaviour.cs
+++ Assets/Scripts/BackgroundBehaviour.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class BackgroundBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public GameObject decoratorPrefab;
+    [Range(0.0f, 5.0f)]
+    public float spawnRate = 1f;
+    [Range(0.0f, 1.0f)]
+    public float spawnRateVariance = 0f;
+    [Range(1, 4)]
+    public int spawnDensity = 2;
+    [Range(0.0f, 1.0f)]
+    public float spawnDensityVariance = 0f;
+
+    private float accumulator = 0f;
+    private float nextSpawnTime = 0f;
+    private int nextSpawnNum = 0;
+
+    private Camera mainCamera;
+
+    // Use this for initialization
+    void Start ()
+    {
+        mainCamera = Camera.main;
+        CalculateNextSpawn(); 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        accumulator += Time.deltaTime;
+        if (accumulator >= nextSpawnTime)
+        {
+            accumulator = 0f;
+
+            var origin = mainCamera.ViewportToWorldPoint(new Vector3(Random.Range(0f, 1f), 1.05f));
+            origin.x = origin.x - (origin.x % 0.05f);
+            origin.y = origin.y - (origin.y % 0.05f);
+            var decoration = Instantiate(decoratorPrefab, origin, transform.rotation) as GameObject;
+            decoration.transform.parent = gameObject.transform;
+
+            var localPosition = decoration.transform.localPosition;
+            localPosition.z = 0;
+            decoration.transform.localPosition = localPosition;
+        }
 	}
+    void CalculateNextSpawn ()
+    {
+        nextSpawnTime = 1.0f / (spawnRate + (Random.Range(-1f * spawnRateVariance, 1f * spawnRateVariance)));
+        nextSpawnNum = spawnDensity + (int) Mathf.Round(Random.Range(-1f * spawnDensityVariance, 1f * spawnDensityVariance));
+    }
 }
---
+++ Assets/Scripts/BackgroundDecoration.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundDecoration : MonoBehaviour {
+
+    public int pixelsPerUnit = 20;
+
+    public Sprite[] decoratorSprites;
+    [Range(0.0f, 5.0f)]
+    public float scrollSpeed = 0.5f;
+
+    private float desiredYTranslate = 0.0f;
+    private float onePixel = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        GetComponent<SpriteRenderer>().sprite = decoratorSprites[Random.Range(0, decoratorSprites.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        onePixel = 1f / pixelsPerUnit;
+        desiredYTranslate += (scrollSpeed * Time.deltaTime);
+        while (desiredYTranslate >= onePixel)
+        {
+            var newPosition = transform.position;
+            newPosition.y = (float) (System.Math.Round((newPosition.y - desiredYTranslate) * pixelsPerUnit, System.MidpointRounding.AwayFromZero) / pixelsPerUnit);
+            transform.position = newPosition;
+            desiredYTranslate -= onePixel;
+        }
+	}
+}
