3d4b4660cd2197ed0ce247176f9c2fffe5f16894
various authors <various.authors@gmail.com>
2015-08-23 21:26:10 -0700

    added color based on damage

--- Assets/Scripts/GoodShip.cs
+++ Assets/Scripts/GoodShip.cs
@@ -7,16 +7,22 @@ public class GoodShip : MonoBehaviour {
     public float m_Health;
 	public int crashDamage;
 
+	private float m_MaxHealth;
+	private Color hale = Color.white;
+	private Color hot = Color.red;
+	private SpriteRenderer spriteRenderer;
+
 	// Use this for initialization
 	void Start () {
-	
+		spriteRenderer = this.GetComponent<SpriteRenderer> ();
+		m_MaxHealth = m_Health;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		spriteRenderer.color = Color.Lerp(this.hale, this.hot, Mathf.Lerp(0, 1, (1 - m_Health/m_MaxHealth)));
 	}
-
+	
 	public void Explode () {
 		GameObject explosion = Instantiate (shipExplosion, this.transform.position, Quaternion.identity) as GameObject;
 
@@ -30,6 +36,7 @@ public class GoodShip : MonoBehaviour {
         if (other.name == "EyeLaser")
         {
             m_Health -= 10.0f * Time.deltaTime;
+
             if ( m_Health <=0 )
             {
                 Explode();
