1f6e2023cfebbdfcdd29a3ecf9a3500ba2018539
Tim Bellefleur <nomoon@phoebus.ca>
2015-08-24 02:00:52 -0700

    Settable status text on EndOfGame primitive.

--- Assets/Scripts/EndOfGameBehaviour.cs
+++ Assets/Scripts/EndOfGameBehaviour.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class EndOfGameBehaviour : MonoBehaviour {
 
+    public Sprite statusText;
+
 	// Use this for initialization
 	void Start () {
-	
+        if (statusText != null) transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = statusText;
 	}
 	
 	// Update is called once per frame
