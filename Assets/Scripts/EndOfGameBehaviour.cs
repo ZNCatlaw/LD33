50ff557bb568d84f16c6b16f82f0da2eb2f2db9c
Tim Bellefleur <nomoon@phoebus.ca>
2015-08-24 11:14:52 -0700

    Add blinking secondary sprite status text to EndOfGame prefab.

--- Assets/Scripts/EndOfGameBehaviour.cs
+++ Assets/Scripts/EndOfGameBehaviour.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class EndOfGameBehaviour : MonoBehaviour {
 
-    public Sprite statusText;
+    public Sprite statusText1;
+    public Sprite statusText2;
 
-	// Use this for initialization
-	void Start () {
-        if (statusText != null) transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = statusText;
-	}
+    // Use this for initialization
+    void Start () {
+        if (statusText1 != null) transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = statusText1;
+        if (statusText2 != null) transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = statusText2;
+    }
 	
 	// Update is called once per frame
 	void Update () {
